0ca2d6071379f43804051a789f5f7c4e7815ce4f
Igor Mucsicska <mucsi_96@yahoo.com>
2015-04-26 11:46:57 +0200

    refactoring JS

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: false, // IMPORTANT, because we do MathJax before markdown,
-    //            however we do escaping in 'CreatePreview'.
-    smartLists: true,
-    smartypants: false
-});
+(function() {
+    'use strict';
 
-function escape(html, encode) {
-    return html
-        .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;');
-}
-
-function runMathJax() {
     var container = document.getElementById('markdown-mathjax');
 
-    container.innerHTML = escape(container.innerHTML);
-    MathJax.Hub.Queue(
-        ["Typeset", MathJax.Hub, container], [mathJaxReady]
-    );
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: false, // IMPORTANT, because we do MathJax before markdown
+        smartLists: true,
+        smartypants: false
+    });
 
-    function mathJaxReady() {
+    function escape() {
+        container.innerHTML = container.innerHTML
+            .replace(/&(?!#?\w+;)/g)
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;');
+    }
+
+    function runMarkDown() {
         var text = container.innerHTML;
 
         text = text.replace(/^&gt;/mg, '>');
         container.innerHTML = marked(text);
+    }
+
+    function markReady() {
         container.classList.add('ready');
     }
-}
 
-runMathJax();
+    MathJax.Hub.Queue([escape], ["Typeset", MathJax.Hub, container], [runMarkDown], [markReady]);
+})();
